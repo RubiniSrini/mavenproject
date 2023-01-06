f580d89d811770f81482216b9de01e9d015b144f
RubiniSrini <srubinirubini17@gmail.com>
2023-01-06 23:30:30 +0530

    maven

---
+++ GitTask.java
@@ -0,0 +1,14 @@
+package com.git.MavenProjectGit;
+
+public class GitTask {
+	private void test() {
+		System.out.println("test");
+	}
+	private void test1() {
+		System.out.println("test1");
+	}
+	private void test2() {
+		System.out.println("test2");
+	}
+
+}
